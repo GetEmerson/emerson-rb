7811bea9f30b0516ccd374ca7e5972929cf9c93b
Corey Innis <corey@coolerator.net>
2012-05-10 13:21:51 -0700

    update emerson.js to version 0.0.3

--- vendor/assets/javascripts/emerson.js
+++ vendor/assets/javascripts/emerson.js
@@ -1,14 +1,12 @@
-//     Emerson.js 0.0.1
+//     Emerson.js 0.0.3
 //
 //     (c) 2012 Corey Innis
 //     Emerson may be freely distributed under the MIT license.
 //     For all details and documentation:
 //     http://coolerator.net
 
-//= require underscore
-//= require emerson/base
-//= require emerson/util
-//= require emerson/http
-//= require emerson/sink
-//= require emerson/view
-// @private
+//     = require emerson/base.js
+//     = require emerson/util.js
+//     = require emerson/http.js
+//     = require emerson/sink.js
+//     = require emerson/view.js
--- vendor/assets/javascripts/emerson/base.js
+++ vendor/assets/javascripts/emerson/base.js
@@ -1,3 +1,7 @@
+//     Emerson Base
+//
+//     Defines the Emerson namespace (global).
+
 (function(){
 
   // Initial Setup
@@ -17,7 +21,7 @@
   }
 
   // Current version of the library. Keep in sync with `package.json`.
-  Emerson.VERSION = '0.0.1';
+  Emerson.VERSION = '0.0.3';
 
   // Reference the base lib (one of jQuery, Zepto or Ender) as $.
   var $ = Emerson.base = (root.jQuery || root.Zepto || root.ender);
@@ -25,9 +29,82 @@
 
   // Primary API
   // --------------------------------------------------------------------------
+
+  // ### .config
+  // Configures the library.
+  //
+  //   * Given an object the current configuration will be extended with that.
+  //   * Given a "dot-notated" string a lookup will be performed.
+  //   * With no argument a copy of the current configuration will be returned.
+  //
+  Emerson.config = function config() {
+    var arg = arguments[0];
+    var result;
+
+    if(arg === undefined) {
+      return _.clone(configuration);
+    }
+
+    if(typeof arg === 'string') {
+      result = configuration;
+
+      try {
+        _.each(arg.split('.'), function(part) {
+          result = result[part];
+        });
+      }
+      catch(e) {
+        return undefined;
+      }
+
+      return _.clone(result);
+    }
+    else {
+      _.extend(configuration, arg);
+    }
+  };
+
+  // ### .init
+  // Initializes the desired Emerson modules.
+  //
+  // By default, will run `.init` for all modules.  If passed specific module
+  // names, it will only initialize those.
   Emerson.init = function init() {
-    _.each(['sink', 'util', 'view'], function(mod) {
+    var modules = ['http', 'sink', 'util', 'view'];
+
+    if(arguments.length) {
+      modules = arguments;
+    }
+
+    _.each(modules, function(mod) {
       Emerson[mod] && Emerson[mod].init();
     });
+  };
+
+
+  // Internal Implementation
+  // --------------------------------------------------------------------------
+
+  // ### configuration
+  // Stores library config, with defaults.
+  //
+  //   * `attrs` are mapped to `[data-attr]` for view/trait selectors.
+  //
+  // Previous (unpublished) versions of this library used `data-presents` to
+  // represent a view and `data-behavior` to represent traits.  This is still
+  // supported by way of:
+  //
+  //     Emerson.config({
+  //       attrs : {
+  //         view   : 'presents',
+  //         traits : 'behavior'
+  //       }
+  //     });
+  //     Emerson.init();
+  var configuration = {
+    attrs : {
+      view   : 'view',
+      traits : 'traits'
+    }
   };
 }).call(this);
